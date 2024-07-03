e7449cc7f1cc40005d6842442d780e2b9b9d386e
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-07-03 11:46:57 +0530

    fgfg

--- app/kids-of-excellence/page.tsx
+++ app/kids-of-excellence/page.tsx
@@ -1,26 +1,27 @@
-import HeroSection from "@/components/[programs_id]/HeroSection";
-import Layout from "@/components/layout";
-import React from "react";
-import Events from "@/components/kids-of-excellence/Events";
-import Guiding from "@/components/kids-of-excellence/Guiding";
-import { getPageContent } from "@/libs/contents/wordpress/data";
-import Loading from "@/app/loading";
+// import HeroSection from "@/components/[programs_id]/HeroSection";
+// import Layout from "@/components/layout";
+// import React from "react";
+// import Events from "@/components/kids-of-excellence/Events";
+// import Guiding from "@/components/kids-of-excellence/Guiding";
+// import { getPageContent } from "@/libs/contents/wordpress/data";
+// import Loading from "@/app/loading";
 
-const page = async () => {
-  const data: KidsOfExcellenceType = await getPageContent("kids-of-excellence");
+// const page = async () => {
+//   const data: KidsOfExcellenceType = await getPageContent("kids-of-excellence");
 
-  if (!data) return <Loading />;;
-  return (
-    <Layout>
-      <HeroSection
-        heroSectionImage={data.page.acf.bannerImage.sourceUrl}
-        title="Kids Of Excellence"
-        subtitle=""
-      />
-      <Guiding data={data} />
-      <Events  data={data}/>
-    </Layout>
-  );
-};
+//   if (!data) return <Loading />;;
+//   return (
+//     <Layout>
+//       <HeroSection
+//         heroSectionImage={data.page.acf.bannerImage.sourceUrl}
+//         title="Kids Of Excellence"
+//         subtitle=""
+//       />
+//       <Guiding data={data} />
+//       <Events  data={data}/>
+//     </Layout>
+//   );
+// };
 
-export default page;
+// export default page;
+
