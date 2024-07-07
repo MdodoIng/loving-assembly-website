145ca02f16b9fb26ff2fcc1cc9a87d6ea948a9c1
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-07-07 12:00:46 +0530

    fd

--- app/[programs_id]/page.tsx
+++ app/[programs_id]/page.tsx
@@ -7,47 +7,30 @@ import { getPageContent } from "@/libs/contents/wordpress/data";
 import descriptionExtractor from "@/libs/hooks/descriptionExtractor";
 import React from "react";
 
-// export async function generateStaticParams() {
-//   const data: MinistriesNavLinksType = await getPageContent(
-//     "ministries-nav-links"
-//   );
-//   const valuesToFilter = ["kids-of-excellence", "man-of-valour"];
-
-//   const filtered = data.utility.acf.items.filter(
-//     (item) => !valuesToFilter.includes(item.link.url)
-//   );
-
-//   return filtered.map((post) => ({
-//     programs_id: post.link.url,
-//   }));
-// }
-
 const page = async ({ params }: any) => {
   const data: MinistriesType = await getPageContent(
     "ministries-by-slug",
     params.programs_id
   );
 
-  if (!data) return <Loading />;
+  const isData = data.pages.nodes[0];
+
+  if (!isData) return <Loading />;
   return (
     <>
       <HeroSection
-        heroSectionImage={
-          data.pages.nodes[0].acf.bannerSection.bannerImage.sourceUrl
-        }
-        subtitle={data.pages.nodes[0].acf.bannerSection.subtitle}
-        title={data.pages.nodes[0].acf.bannerSection.title}
-        statement={data.pages.nodes[0].acf.bannerSection.subtitle}
+        heroSectionImage={isData?.acf.bannerSection.bannerImage.sourceUrl}
+        subtitle={isData?.acf.bannerSection.subtitle}
+        title={isData?.acf.bannerSection.title}
+        statement={isData?.acf.bannerSection.subtitle}
       />
 
       <About
-        logo={data.pages.nodes[0].acf.firstSection.icon.sourceUrl}
-        description={descriptionExtractor(
-          data.pages.nodes[0].acf.firstSection.content
-        )}
+        logo={isData?.acf.firstSection.icon.sourceUrl}
+        description={descriptionExtractor(isData?.acf.firstSection.content)}
         button={{
-          title: data.pages.nodes[0].acf.firstSection.buttonText,
-          link: data.pages.nodes[0].acf.firstSection.buttonLink,
+          title: isData?.acf.firstSection.buttonText,
+          link: isData?.acf.firstSection.buttonLink,
         }}
       />
     </>
